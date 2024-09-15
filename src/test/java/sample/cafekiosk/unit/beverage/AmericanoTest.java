4f83aeb1c1e7a26890b0f7a132ce5b34473cb2f0
neo <maiload77@gmail.com>
2024-09-15 13:53:51 +0900

    Part 5. BDD 스타일로 작성하기

---
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,25 @@
+package sample.cafekiosk.unit.beverage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+class AmericanoTest {
+
+    @Test
+    void getName() {
+        Americano americano = new Americano();
+
+        assertEquals(americano.getName(), "아메리카노"); // JUnit
+        assertThat(americano.getName()).isEqualTo("아메리카노"); // AssertJ
+    }
+
+    @Test
+    void getPrice() {
+        Americano americano = new Americano();
+
+        assertThat(americano.getPrice()).isEqualTo(4000);
+    }
+}
